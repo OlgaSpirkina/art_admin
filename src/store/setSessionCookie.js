f6e36a6086036435d0dcf6f820965cf42eb76ca7
Olga <>
2023-08-18 23:05:40 +0200

    finished admin auth using redux toolkit

--- src/store/setSessionCookie.js
+++
@@ -1,6 +0,0 @@
-import Cookies from "js-cookie";
-const setSessionCookie = (token) => {
-    const expirationTime = new Date(Date.now() + 7200000); 
-    Cookies.set('authToken', token, { expires: expirationTime});
-};
-export default setSessionCookie;
